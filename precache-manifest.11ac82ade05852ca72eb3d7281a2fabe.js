01ec6260878c70a39b1167331dd907a2370e6993
Siddharth Chandran <siddharth.chandrane@gmail.com>
2020-04-18 19:44:47 +0530

    Updates

---
+++ precache-manifest.11ac82ade05852ca72eb3d7281a2fabe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "010c1b4bcf1bcf6ad7660313c8ce90a9",
+    "url": "/sid/index.html"
+  },
+  {
+    "revision": "092e5dd3cdec1226277c",
+    "url": "/sid/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7213753bab049fe4d0a8",
+    "url": "/sid/static/js/2.cf758feb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sid/static/js/2.cf758feb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092e5dd3cdec1226277c",
+    "url": "/sid/static/js/main.c539dd15.chunk.js"
+  },
+  {
+    "revision": "ba393fd83510a8c01853",
+    "url": "/sid/static/js/runtime-main.9a44438c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sid/static/media/logo.5d5d9eef.svg"
+  }
+]);
